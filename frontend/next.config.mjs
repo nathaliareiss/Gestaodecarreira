209b0578631471486fbd2860206d9b2401ca221d
Nathalia <alencarnrr@gmail.com>
2026-04-30 17:06:13 -0300

    feat: atualiza importação de rotas e remove configurações experimentais do Next.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,13 +4,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    workerThreads: true,
-    cpus: 1,
-    staticGenerationMaxConcurrency: 1,
-    staticGenerationMinPagesPerWorker: 1,
-    webpackBuildWorker: false,
-  },
 }
 
 export default nextConfig
